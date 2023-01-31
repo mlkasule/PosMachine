08d4834008cafffcb744e2e79d7555c11cacbdc6
casulemarc <casulemarc2014@gmail.com>
2023-01-31 17:23:54 -0500

    Initial commit

---
+++ PosMachine.java
@@ -0,0 +1,8 @@
+/*
+A software company sells a package at the regular price of $99. Quantity discounts are given according to the following table:
+Write a program that asks the user to enter the number of packages purchased. The program should then display the amount of the discount (if any) and the total amount of the purchase after the discount.
+Hint: To calculate 20% of a value N, you can use the formula 0.2 * N.
+Look carefully at the following sample run of the program. In particular, notice the wording of the messages and the placement of spaces and colons. Your program's output must match this.
+*/
+public class PosMachine {
+}
